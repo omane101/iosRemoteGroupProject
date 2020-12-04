14736b6d9e3442f32cd3005d925c0dc8fc67a901
ahmedmoussa <elyarmuk@hotmail.com>
2020-12-04 01:18:24 -0800

    fix the storyboard conflict

--- Yelpy/AppDelegate.swift
+++ Yelpy/AppDelegate.swift
@@ -17,17 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-<<<<<<< HEAD
-        let parseConfig = ParseClientConfiguration {
-                        $0.applicationId = "3GhjILvjyKyp3SxETOMq0ae8UyBYxTES6NHOHcHQ" // <- UPDATE
-                        $0.clientKey = "4XsXIXZWl3wDem5Ijbj5odYMvVRkwZk4WIBYr3sB" // <- UPDATE
-                        $0.server = "https://parseapi.back4app.com"
-                }
-                Parse.initialize(with: parseConfig)
-                
-=======
+
         
->>>>>>> login
 
         FirebaseApp.configure()
     
